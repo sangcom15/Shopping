e06475532cc96ea0faf4a61204d443e96299535a
sangcom15 <sangcom15@naver.com>
2018-05-27 21:01:36 +0900

    예제 추가

---
+++ src/components/pages/List.vue
@@ -0,0 +1,190 @@
+<template>
+    <!-- <div id="list" class="listarea">
+        {{ message }}
+    </div> -->
+
+
+
+
+
+    <main class="main-wrapper" id="container"> 
+        <!-- image Gallery -->
+        
+        <div class="wrapper">
+            <div class="">
+            <ul class="small-block-grid-2 medium-block-grid-3 large-block-grid-5 masonry">
+                <li class="masonry-item grid">
+                <figure class="effect-sarah"> <img src="../../assets/images/gallery/01.jpg" alt="NoImage" />
+                    <figcaption>
+                    <h2>Photo <span>Title</span></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <a href="details.html">View more</a> </figcaption>
+                </figure>
+                </li>
+                <li class="masonry-item grid">
+                <figure class="effect-sarah"> <img src="../../assets/images/gallery/02.jpg" alt="" />
+                    <figcaption>
+                    <h2>Photo <span>Title</span></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <a href="details.html">View more</a> </figcaption>
+                </figure>
+                </li>
+                <li class="masonry-item grid">
+                <figure class="effect-sarah"> <img src="../../assets/images/gallery/03.jpg" alt="" />
+                    <figcaption>
+                    <h2>Photo <span>Title</span></h2>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                    <a href="details.html">View more</a> </figcaption>
+                </figure>
+                </li>
+                <li class="masonry-item grid">
+                <figure class="effect-sarah"> <img src="../../assets/images/gallery/04.jpg" alt="" />
+                    <figcaption>
+                    <h2>Photo <span>Title</span></h2>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                    <a href="details.html">View more</a> </figcaption>
+                </figure>
+                </li>
+                <li class="masonry-item grid">
+                <figure class="effect-sarah"> <img src="../../assets/images/gallery/05.jpg" alt="" />
+                    <figcaption>
+                    <h2>Photo <span>Title</span></h2>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                    <a href="details.html">View more</a> </figcaption>
+                </figure>
+                </li>
+                <li class="masonry-item grid">
+                <figure class="effect-sarah"> <img src="../../assets/images/gallery/06.jpg" alt="" />
+                    <figcaption>
+                    <h2>Photo <span>Title</span></h2>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                    <a href="details.html">View more</a> </figcaption>
+                </figure>
+                </li>
+                <li class="masonry-item grid">
+                <figure class="effect-sarah"> <img src="../../assets/images/gallery/07.jpg" alt="" />
+                    <figcaption>
+                    <h2>Photo <span>Title</span></h2>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                    <a href="details.html">View more</a> </figcaption>
+                </figure>
+                </li>
+                <li class="masonry-item grid">
+                <figure class="effect-sarah"> <img src="../../assets/images/gallery/08.jpg" alt="" />
+                    <figcaption>
+                    <h2>Photo <span>Title</span></h2>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                    <a href="details.html">View more</a> </figcaption>
+                </figure>
+                </li>
+                <li class="masonry-item grid">
+                <figure class="effect-sarah"> <img src="../../assets/images/gallery/09.jpg" alt="" />
+                    <figcaption>
+                    <h2>Photo <span>Title</span></h2>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                    <a href="details.html">View more</a> </figcaption>
+                </figure>
+                </li>
+                <li class="masonry-item grid">
+                <figure class="effect-sarah"> <img src="../../assets/images/gallery/10.jpg" alt="" />
+                    <figcaption>
+                    <h2>Photo <span>Title</span></h2>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                    <a href="details.html">View more</a> </figcaption>
+                </figure>
+                </li>
+                <li class="masonry-item grid">
+                <figure class="effect-sarah"> <img src="../../assets/images/gallery/11.jpg" alt="" />
+                    <figcaption>
+                    <h2>Photo <span>Title</span></h2>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                    <a href="details.html">View more</a> </figcaption>
+                </figure>
+                </li>
+                <li class="masonry-item grid">
+                <figure class="effect-sarah"> <img src="../../assets/images/gallery/12.jpg" alt="" />
+                    <figcaption>
+                    <h2>Photo <span>Title</span></h2>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                    <a href="details.html">View more</a> </figcaption>
+                </figure>
+                </li>
+                <li class="masonry-item grid">
+                <figure class="effect-sarah"> <img src="../../assets/images/gallery/13.jpg" alt="" />
+                    <figcaption>
+                    <h2>Photo <span>Title</span></h2>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                    <a href="details.html">View more</a> </figcaption>
+                </figure>
+                </li>
+                <li class="masonry-item grid">
+                <figure class="effect-sarah"> <img src="../../assets/images/gallery/14.jpg" alt="" />
+                    <figcaption>
+                    <h2>Photo <span>Title</span></h2>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                    <a href="details.html">View more</a> </figcaption>
+                </figure>
+                </li>
+                
+                <li class="masonry-item grid">
+                <figure class="effect-sarah"> <img src="../../assets/images/gallery/16.jpg" alt="" />
+                    <figcaption>
+                    <h2>Photo <span>Title</span></h2>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                    <a href="details.html">View more</a> </figcaption>
+                </figure>
+                </li>
+                <li class="masonry-item grid">
+                <figure class="effect-sarah"> <img src="../../assets/images/gallery/17.jpg" alt="" />
+                    <figcaption>
+                    <h2>Photo <span>Title</span></h2>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                    <a href="details.html">View more</a> </figcaption>
+                </figure>
+                </li>
+                <li class="masonry-item grid">
+                <figure class="effect-sarah"> <img src="../../assets/images/gallery/18.jpg" alt="" />
+                    <figcaption>
+                    <h2>Photo <span>Title</span></h2>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                    <a href="details.html">View more</a> </figcaption>
+                </figure>
+                </li>
+                <li class="masonry-item grid">
+                <figure class="effect-sarah"> <img src="../../assets/images/gallery/19.jpg" alt="" />
+                    <figcaption>
+                    <h2>Photo <span>Title</span></h2>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                    <a href="details.html">View more</a> </figcaption>
+                </figure>
+                </li>
+                <li class="masonry-item grid">
+                <figure class="effect-sarah"> <img src="../../assets/images/gallery/20.jpg" alt="" />
+                    <figcaption>
+                    <h2>Photo <span>Title</span></h2>
+                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                    <a href="details.html">View more</a> </figcaption>
+                </figure>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  components: {
+  },
+  data () {
+    return {
+      message: 'List'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
